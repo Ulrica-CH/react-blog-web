1a0298541fa5fcaa9083439f5673f633be7b5379
xiaoyang <michstabe515@163.com>
2023-04-21 16:30:41 +0800

    feat: add stylelit

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,15 @@
 // craco.config.js
 const path = require('path');
+const cracoSassResourcesLoader = require('sass-resources-loader');
 module.exports = {
+  plugins: [
+    {
+      plugin: cracoSassResourcesLoader,
+      options: {
+        resources: path.resolve(__dirname, 'src/styles/index.scss'),
+      },
+    },
+  ],
   webpack: {
     alias: {
       '@': path.join(__dirname, 'src'), // 允许通过@符号来表示 src目录
